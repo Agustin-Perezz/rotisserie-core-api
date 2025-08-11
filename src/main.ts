7186c512e85d7c7855e0601ee8f11db0d97b52a7
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-08-11 09:16:10 -0300

    Configure cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
