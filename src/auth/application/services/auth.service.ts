e855e5fad602b98ee15575a338391f0f0f1dc989
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-09-02 15:24:19 -0300

    Update config service import in auth service

--- src/auth/application/services/auth.service.ts
+++ src/auth/application/services/auth.service.ts
@@ -1,11 +1,15 @@
 import { AuthServiceInterface } from '@auth/domain/interfaces/auth-service.interface';
 import { AuthCredentials } from '@auth/domain/interfaces/types/auth-credentials';
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { UsersService } from '@users/application/services/users.service';
 
 @Injectable()
 export class AuthService implements AuthServiceInterface {
-  constructor(public usersService: UsersService) {}
+  constructor(
+    public usersService: UsersService,
+    @Inject(ConfigService) private readonly configService: ConfigService,
+  ) {}
 
   async signUp(credentials: AuthCredentials): Promise<void> {
     const user = await this.usersService.findOne(credentials.firebaseUid);
