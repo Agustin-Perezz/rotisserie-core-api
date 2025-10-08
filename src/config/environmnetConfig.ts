ffd603775c152838ea7c2e0f69fb1beeb33bd776
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-10-08 07:56:02 -0300

    Remove access token env

--- src/config/environmnetConfig.ts
+++ src/config/environmnetConfig.ts
@@ -2,7 +2,6 @@ export const environmentConfig = () => ({
   mercadoPago: {
     mpApi: process.env.MP_API,
     authApi: process.env.MP_AUTH_API,
-    accessToken: process.env.MP_ACCESS_TOKEN,
     clientId: process.env.MP_CLIENT_ID,
     clientSecret: process.env.MP_CLIENT_SECRET,
     redirectUri: process.env.MP_REDIRECT_URI,
