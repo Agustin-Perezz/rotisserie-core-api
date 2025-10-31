c0af56cf1913fa1c1acd2d3b6da26d3a78e76612
Agustin-Perezz <agustinnicolas806@gmail.com>
2025-10-31 10:06:23 -0300

    Make get shop by name endpoint public

--- src/shop/infrastructure/controllers/shop.controller.ts
+++ src/shop/infrastructure/controllers/shop.controller.ts
@@ -16,16 +16,17 @@ import { CreateShopDto } from '../../application/dto/create-shop.dto';
 import { UpdateShopDto } from '../../application/dto/update-shop.dto';
 import { ShopService } from '../../application/services/shop.service';
 
-@UseGuards(AuthGuard)
 @Controller('shops')
 export class ShopController {
   constructor(private readonly shopService: ShopService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.shopService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.shopService.findOne(id);
@@ -36,22 +37,26 @@ export class ShopController {
     return this.shopService.findByName(name);
   }
 
+  @UseGuards(AuthGuard)
   @Get('owner/:ownerId')
   findByOwnerId(@Param('ownerId') ownerId: string) {
     return this.shopService.findByOwnerId(ownerId);
   }
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() shop: CreateShopDto, @UserId() userId: string) {
     shop.ownerId = userId;
     return this.shopService.create(shop);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() shop: UpdateShopDto) {
     return this.shopService.update(id, shop);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   delete(@Param('id') id: string) {
     return this.shopService.delete(id);
